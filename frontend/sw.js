ea48598682207752bfc870e4f321db4881bb334c
PixelCode01 <105023758+PixelCode01@users.noreply.github.com>
2026-07-31 15:40:00 +0530

    add pwa manifest and offline service worker

---
+++ frontend/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'ppa-cache-v2'
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/assets/images/logo.svg',
+  '/assets/images/landing_hero.png'
+]
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting()
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    }).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('/api/')) {
+    return
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
